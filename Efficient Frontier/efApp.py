a6cab94a946f2a152ec0e3423a36e7c7658559a0
Atanas Delevski <adelevski@gmail.com>
2021-05-28 17:41:43 -0500

    update

--- Efficient Frontier/efApp.py
+++ Efficient Frontier/efApp.py
@@ -1,12 +1,14 @@
 import numpy as np
 import datetime as dt
+import pandas as pd
 from pandas_datareader import data as pdr
 import scipy.optimize as sc
+
 
 # Importing data
 def getData(stocks, start, end):
     stockData = pdr.get_data_yahoo(stocks, start=start, end=end)
-    stockData = stockData['Close']
+    stockData = stockData['Adj Close']
 
     returns = stockData.pct_change()
     meanReturns = returns.mean()
@@ -15,8 +17,8 @@ def getData(stocks, start, end):
 
 # Mean returns and standard deviations
 def portfolioPerformance(weights, meanReturns, covMatrix):
-    returns = np.sum(meanReturns*weights)*365
-    std = np.sqrt( np.dot(weights.T, np.dot(covMatrix, weights)) ) * np.sqrt(365)
+    returns = np.sum(meanReturns*weights)*252
+    std = np.sqrt( np.dot(weights.T, np.dot(covMatrix, weights)) ) * np.sqrt(252)
     return returns, std
 
 def negativeSR(weights, meanReturns, covMatrix, riskFreeRate=0):
@@ -49,29 +51,51 @@ def minVariance(meanReturns, covMatrix, riskFreeRate=0, constraintSet=(0,1)):
 
 
 ####  Script
-stocks = ['BTC-USD', 'ETH-USD', 'ADA-USD']
+stocks = ['AAPL', 'TSLA', 'MSFT']
 
 # Time (2 years back)
 endDate = dt.datetime.now()
 startDate = endDate - dt.timedelta(days=365)
 
-weights = np.array([0.3, 0.3, 0.4])
+# weights = np.array([0.3, 0.3, 0.4])
 
 meanReturns, covMatrix = getData(stocks, start=startDate, end=endDate)
-returns, std = portfolioPerformance(weights, meanReturns, covMatrix)
+# returns, std = portfolioPerformance(weights, meanReturns, covMatrix)
 
-result = maxSR(meanReturns, covMatrix)
-maxSR, maxWeights = result['fun'], result['x']
-print(round(maxSR, 3), [round(x, 3) for x in maxWeights])
+# result = maxSR(meanReturns, covMatrix)
+# maxSR, maxWeights = result['fun'], result['x']
+# print(round(maxSR, 3), [round(x, 3) for x in maxWeights])
 
-minVarResult = minVariance(meanReturns, covMatrix)
-minVar, minVarWeights = minVarResult['fun'], minVarResult['x']
-print(round(minVar, 3), [round(x, 3) for x in minVarWeights])
+# minVarResult = minVariance(meanReturns, covMatrix)
+# minVar, minVarWeights = minVarResult['fun'], minVarResult['x']
+# print(round(minVar, 3), [round(x, 3) for x in minVarWeights])
 
-
+def efficientOpt(meanReturns, covMatrix, returnTarget, constraintSet=(0,1)):
+    """
+    For each returnTarget, we want to optimize the portfolio for min variance
+    """
 
 def calculatedResults(meanReturns, covMatrix, riskFreeRate=0, constraintSet=(0,1)):
     """
     Read in mean, cov matrix, and other financial infromation
     Output max Sharpe ratio, min Volati
     """
+    # Max Sharpe Ratio Portfolio
+    maxSR_Portfolio = maxSR(meanReturns, covMatrix)
+    maxSR_returns, maxSR_std = portfolioPerformance(maxSR_Portfolio['x'], meanReturns, covMatrix)
+    maxSR_returns, maxSR_std = round(maxSR_returns*100,2), round(maxSR_std*100,2)
+    maxSR_allocation = pd.DataFrame(maxSR_Portfolio['x'], index=meanReturns.index, columns=['allocation'])
+    maxSR_allocation.allocation = [round(i*100, 0) for i in maxSR_allocation.allocation]
+
+    # Min Volatility Portfolio
+    minVol_Portfolio = minVariance(meanReturns, covMatrix)
+    minVol_returns, minVol_std = portfolioPerformance(minVol_Portfolio['x'], meanReturns, covMatrix)
+    minVol_returns, minVol_std = round(minVol_returns*100,2), round(minVol_std*100, 2)
+    minVol_allocation = pd.DataFrame(minVol_Portfolio['x'], index=meanReturns.index, columns=['allocation'])
+    minVol_allocation.allocation = [round(i*100, 0) for i in minVol_allocation.allocation]
+
+    return maxSR_returns, maxSR_std, maxSR_allocation, minVol_returns, minVol_std, minVol_allocation
+
+
+print(calculatedResults(meanReturns, covMatrix))
+
